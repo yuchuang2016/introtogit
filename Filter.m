93c0401638ca302392026323706e49052b9a1b00
yuchuang2016 <32988818+yuchuang2016@users.noreply.github.com>
2018-12-04 08:57:24 -0600

    Delete Filter.m

--- Filter.m
+++
@@ -1,5 +0,0 @@
-bhi = firls(18,[0 0.45 0.55 1],[1 1 0 0],[1 100]);
-blo = firls(18,[0 0.45 0.55 1],[1 1 0 0],[100 1]);
-b = firls(18,[0 0.45 0.55 1],[1 1 0 0],[1 1]);
-hfvt = fvtool(bhi,1,blo,1,b,1,'MagnitudeDisplay','Zero-phase');
-legend(hfvt,'bhi: w = [1 100]','blo: w = [100 1]','b: w = [1 1]')
